03909fef0eb1cf1dec2fcd9c9969db5d0baeaf84
mesteranas <135442710+mesteranas@users.noreply.github.com>
2024-03-12 00:35:43 +0200

    12/03/2024

---
+++ systemInfo/main.py
@@ -0,0 +1,93 @@
+import sys
+from custome_errors import *
+sys.excepthook = my_excepthook
+import subprocess
+import update
+import gui
+import guiTools
+from settings import *
+import PyQt6.QtWidgets as qt
+import PyQt6.QtGui as qt1
+import PyQt6.QtCore as qt2
+language.init_translation()
+class main (qt.QMainWindow):
+    def __init__(self):
+        super().__init__()
+        self.setWindowTitle(app.name + _("version : ") + str(app.version))
+        layout=qt.QVBoxLayout()
+        self.serial=qt.QPushButton(_("serial number"))        
+        self.serial.clicked.connect(lambda:guiTools.TextViewer(self,_("serial number"),self.get(['wmic', 'bios', 'get', 'serialnumber']).strip().split("\n")[2].strip()).exec())
+        self.serial.setDefault(True)
+        layout.addWidget(self.serial)
+        self.windowsKey=qt.QPushButton(_("windows key"))
+        self.windowsKey.clicked.connect(lambda:guiTools.TextViewer(self,_("windows key"),self.get(['wmic', 'path', 'SoftwareLicensingService', 'get', 'OA3xOriginalProductKey']).strip().split("\n")[2].strip()).exec())
+        self.windowsKey.setDefault(True)
+        layout.addWidget(self.windowsKey)
+        self.other=qt.QPushButton(_("other"))
+        self.other.clicked.connect(lambda:guiTools.TextViewer(self,_("other"),self.get("systeminfo")).exec())
+        self.other.setDefault(True)
+        layout.addWidget(self.other)
+        self.setting=qt.QPushButton(_("settings"))
+        self.setting.setDefault(True)
+        self.setting.clicked.connect(lambda: settings(self).exec())
+        layout.addWidget(self.setting)
+        w=qt.QWidget()
+        w.setLayout(layout)
+        self.setCentralWidget(w)
+
+        mb=self.menuBar()
+        help=mb.addMenu(_("help"))
+        helpFile=qt1.QAction(_("help file"),self)
+        help.addAction(helpFile)
+        helpFile.triggered.connect(lambda:guiTools.HelpFile())
+        helpFile.setShortcut("f1")
+        cus=help.addMenu(_("contact us"))
+        telegram=qt1.QAction("telegram",self)
+        cus.addAction(telegram)
+        telegram.triggered.connect(lambda:guiTools.OpenLink(self,"https://t.me/mesteranasm"))
+        telegramc=qt1.QAction(_("telegram channel"),self)
+        cus.addAction(telegramc)
+        telegramc.triggered.connect(lambda:guiTools.OpenLink(self,"https://t.me/tprogrammers"))
+        githup=qt1.QAction(_("Github"),self)
+        cus.addAction(githup)
+        githup.triggered.connect(lambda: guiTools.OpenLink(self,"https://Github.com/mesteranas"))
+        X=qt1.QAction(_("x"),self)
+        cus.addAction(X)
+        X.triggered.connect(lambda:guiTools.OpenLink(self,"https://x.com/mesteranasm"))
+        email=qt1.QAction(_("email"),self)
+        cus.addAction(email)
+        email.triggered.connect(lambda: guiTools.sendEmail("anasformohammed@gmail.com","project_type=GUI app={} version={}".format(app.name,app.version),""))
+        Github_project=qt1.QAction(_("visite project on Github"),self)
+        help.addAction(Github_project)
+        Github_project.triggered.connect(lambda:guiTools.OpenLink(self,"https://Github.com/mesteranas/{}".format(settings_handler.appName)))
+        Checkupdate=qt1.QAction(_("check for update"),self)
+        help.addAction(Checkupdate)
+        Checkupdate.triggered.connect(lambda:update.check(self))
+        licence=qt1.QAction(_("license"),self)
+        help.addAction(licence)
+        licence.triggered.connect(lambda: Licence(self))
+        donate=qt1.QAction(_("donate"),self)
+        help.addAction(donate)
+        donate.triggered.connect(lambda:guiTools.OpenLink(self,"https://www.paypal.me/AMohammed231"))
+        about=qt1.QAction(_("about"),self)
+        help.addAction(about)
+        about.triggered.connect(lambda:qt.QMessageBox.information(self,_("about"),_("{} version: {} description: {} developer: {}").format(app.name,str(app.version),app.description,app.creater)))
+        self.setMenuBar(mb)
+        if settings_handler.get("update","autoCheck")=="True":
+            update.check(self,message=False)
+    def closeEvent(self, event):
+        if settings_handler.get("g","exitDialog")=="True":
+            m=guiTools.ExitApp(self)
+            m.exec()
+            if m:
+                event.ignore()
+        else:
+            self.close()
+    def get(self,commend):
+        result=subprocess.run(commend,capture_output=True,text=True)
+        return result.stdout
+App=qt.QApplication([])
+w=main()
+w.show()
+App.setStyle('fusion')
+App.exec()
---
+++ systemInfo/settings/app.py
@@ -0,0 +1,6 @@
+name="system info"
+appName="systemInfo_GUI"
+appdirname="systemInfo"
+version=1.0
+description="a tool to get some system information"
+creater="mesteranas"
